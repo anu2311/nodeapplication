eea5d31028f3c3e4bca8ac58952375a91b2fad64
anu <anusreesudhanpk@gmail.com>
2022-04-24 18:05:36 +0530

    commit1

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "245ca63a0c08a35042a2af6f268dce06"
+    }
+}
+
+module.exports = constants;
+
